43bfa62e03c12e346ad52ad96054bf8b7ff166cf
Vishal Tanawade <vishal.tanawade@emerson.com>
2025-07-20 09:33:05 +0530

    Added angular toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatToolbarModule, MatIconModule, MatButtonModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
